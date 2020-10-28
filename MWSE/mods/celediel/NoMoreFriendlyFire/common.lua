0039f188b28be400d316cb305e71336622675c5c
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2020-10-27 21:57:34 -0700

    I guess that's a version bump although I probably won't even bother uploading this verson to the nexus

--- MWSE/mods/celediel/NoMoreFriendlyFire/common.lua
+++ MWSE/mods/celediel/NoMoreFriendlyFire/common.lua
@@ -6,7 +6,7 @@ this.modConfig = this.modName:gsub("%s", "")
 this.modInfo = "Stop friendly fire. Player companions can't damage the player, the player " ..
                "can't damage companions, and companions can't damage each other. That's it."
 this.author = "Celediel"
-this.version = "1.5.0"
+this.version = "1.5.1"
 
 -- debug
 this.logLevels = {no = 0, small = 1, big = 2}
